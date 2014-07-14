82fbd8977b16e5ef9e5ea140bc5ed0b474e82ba1
mattwd7 <mattwd7@mattdick.att.net>
2014-07-13 18:31:11 -0700

    Initial Commit

---
+++ ToDoListObjC/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoListObjC
+//
+//  Created by mattwd7 on 7/13/14.
+//  Copyright (c) 2014 Matt Dick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
